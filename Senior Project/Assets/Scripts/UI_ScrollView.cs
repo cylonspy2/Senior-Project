cb5a5032bb23209e15afd0825d230027f66c631f
Li Guan <sdplguanli@gmail.com>
2020-11-19 20:58:42 -0500

    Add a scroll function to dropdown menu

---
+++ Senior Project/Assets/Scripts/UI_ScrollView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ScrollView : MonoBehaviour
+{
+    public Vector2 startPos;
+    public Vector2 endPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
