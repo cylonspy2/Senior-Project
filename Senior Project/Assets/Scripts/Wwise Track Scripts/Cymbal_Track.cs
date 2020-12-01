1d338e436e5243b20783ef4e168a8915222d4090
VirtualTitan <superrrgaf@yahoo.com>
2020-12-01 06:24:57 -0500

    Integration and Demonstration of Wwise
    -Integrated Wwise
    
    -Added several temporary "music boxes" as placeholders
    
    -Added code to enable music tracks to play when different objects are generated in scene.

---
+++ Senior Project/Assets/Scripts/Wwise Track Scripts/Cymbal_Track.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cymbal_Track : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.SetRTPCValue("Controller_Cymbal", 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
