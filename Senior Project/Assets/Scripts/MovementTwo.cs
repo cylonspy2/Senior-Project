749af3afc13399b8ae663deb0b804191e7a4f24d
mikegiorgianni <mgiorgianni98@gmail.com>
2020-12-03 11:15:28 -0500

    tankScale with fish movement
    I made the fish tank scale with the press of numpad keys 1,2 for x and 4, 5 for y and the fish area of movement scales with the tank size.

--- Senior Project/Assets/Scripts/MovementTwo.cs
+++ Senior Project/Assets/Scripts/MovementTwo.cs
@@ -13,6 +13,9 @@ public class MovementTwo : MonoBehaviour
     public float timerForNewPos;
     bool InCoRoutine;
     Vector3 target;
+    public float xa = 4.5f;
+    public float ya = 4.5f;
+    public float za = 4.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +25,13 @@ public class MovementTwo : MonoBehaviour
 
     Vector3 getNewRandomPosition()
     {
-        float x = Random.Range(-4.5f, 4.5f);
-        float y = Random.Range(-4.5f, 4.5f);
-        float z = Random.Range(-4.5f, 4.5f);
+        float xb = xa * -1;
+        float yb = ya * -1;
+        float zb = za * -1;
+
+        float x = Random.Range(xb, xa);
+        float y = Random.Range(yb, ya);
+        float z = Random.Range(zb, za);
 
         Vector3 pos = new Vector3(x, y, z);
         return pos;
@@ -48,6 +55,18 @@ public class MovementTwo : MonoBehaviour
     
     void Update()
     {
+        if (Input.GetKey (KeyCode.Keypad0))
+            xa += -0.05f;
+
+        if (Input.GetKey (KeyCode.Keypad1))
+            xa += 0.05f;
+
+        if (Input.GetKey (KeyCode.Keypad4))
+            ya += -0.05f; 
+
+        if (Input.GetKey (KeyCode.Keypad5))
+            ya += 0.05f;
+
         if (!InCoRoutine)
             StartCoroutine(Coroutine());
     }
---
+++ Senior Project/Assets/Scripts/TankScale.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.Keypad0))
+        { 
+            gameObject.transform.localScale += new Vector3 (-0.05f, 0.0f, 0.0f);
+        } 
+        if (Input.GetKey (KeyCode.Keypad1))
+        { 
+            gameObject.transform.localScale += new Vector3 (0.05f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey (KeyCode.Keypad4))
+        { 
+            gameObject.transform.localScale += new Vector3 (0.0f, 0.0f, -0.05f);
+        } 
+        if (Input.GetKey (KeyCode.Keypad5))
+        { 
+            gameObject.transform.localScale += new Vector3 (0.0f, 0.0f, 0.05f);
+        }
+        /*if (Input.GetKeyDown("Keypad0")) 
+        {
+            GameObject.transform.localScale.x += new Vector3(-0.01f, 0.0f, 0.0f) * Time.deltaTime;
+        }*/
+    }
+}
