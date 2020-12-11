f60e7dacac67ad1061314b5267d19b35dbb61556
VirtualTitan <superrrgaf@yahoo.com>
2020-12-10 22:26:44 -0500

    Final Bleh

---
+++ Senior Project/Assets/Scripts/Wwise Track Scripts/Cowbell1_Track.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cowbell1_Track : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.SetRTPCValue("Controller_Cowbell1", 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
