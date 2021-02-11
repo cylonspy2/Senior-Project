756e09e6496445cc8a2bc092e5a61032a6a4c02d
Li Guan <sdplguanli@gmail.com>
2021-02-11 14:55:04 -0500

    New UI Layout
    Fish re spawning need to work with mouse button

---
+++ Senior Project/Assets/Scripts/RightClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RightClick : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject image1;
+    public GameObject image2;
+
+    public GameObject deletedUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("right");
+            deletedUI.SetActive(false);
+
+            image1.GetComponent<Image>().color = new Color(255, 255, 255, 0.2f);
+            image2.GetComponent<Image>().color = new Color(255, 255, 255, 0.2f);
+        }
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            image1.GetComponent<Image>().color = new Color(255, 255, 255, 1f);
+            image2.GetComponent<Image>().color = new Color(255, 255, 255, 1f);
+        }
+    }
+}
---
+++ Senior Project/Assets/Scripts/SelectDecorations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDecorations : MonoBehaviour
+{
+    public GameObject box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TurnOnBox()
+    {
+        box.SetActive(true);
+    }
+    public void TurnOffBox()
+    {
+        box.SetActive(false);
+    }
+}
