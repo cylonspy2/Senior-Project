b0a7ebad233abfc0e892b704da53cbd0cd3e22e1
mikegiorgianni <mgiorgianni98@gmail.com>
2021-02-12 11:52:31 -0500

    Overlord Music
    Controls the music for it is the overlord

---
+++ Senior Project/Assets/Scripts/Wwise Track Scripts/Overlord_Music.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//VI_Cowbell01
+//VI_Cowbell02
+//VI_BassDrum01
+//VI_BassDrum02 There are two BassDrum01 scripts Bass_Track && BDrum_Track
+//VI_Chords01
+//VI_Cymbal01
+//VI_SnareDrum01
+//VI_Solo01
+
+public class Overlord_Music : MonoBehaviour
+{
+    public MetronomeScript meta;
+    bool hasStarted = false;
+    public string selectedMusic;
+
+    void Start()
+    {
+        GameObject manager = GameObject.Find("GameManager");
+        meta = manager.GetComponent<MetronomeScript>();
+    }
+
+    void Update()
+    {
+        if (meta.Tick == true && hasStarted == false)
+        {
+            AkSoundEngine.PostEvent(selectedMusic, gameObject);
+            hasStarted = true;
+        }
+    }
+}
