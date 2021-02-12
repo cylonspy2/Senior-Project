183cabc50a6c9cd11abf84fbcdc7aa9f23cf46ba
VirtualTitan <superrrgaf@yahoo.com>
2021-02-12 16:40:39 -0500

    Wwise Stop Sounds
    - Added support to be able to Stop sounds and music from playing with Wwise

---
+++ Senior Project/Assets/Scripts/Wwise Track Scripts/resetTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.PostEvent("reset_All", gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
