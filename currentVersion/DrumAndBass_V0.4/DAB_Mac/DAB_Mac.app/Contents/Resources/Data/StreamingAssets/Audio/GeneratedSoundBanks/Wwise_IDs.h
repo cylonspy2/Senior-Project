69382538911dc45176430d47602fb6da1d8433f7
cylonspy2 <53708326+cylonspy2@users.noreply.github.com>
2021-02-26 03:52:39 -0500

    playtest build prepared
    Added new build
    Updated visuals

---
+++ currentVersion/DrumAndBass_V0.4/DAB_Mac/DAB_Mac.app/Contents/Resources/Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID GAME_START_EVENT = 3212575115U;
+        static const AkUniqueID RESET_ALL = 2081023666U;
+        static const AkUniqueID RESET_BASS01 = 1309399501U;
+        static const AkUniqueID RESET_BASSDRUM01 = 365945877U;
+        static const AkUniqueID RESET_CHORDS01 = 1851298689U;
+        static const AkUniqueID RESET_COWBELL01 = 1735431224U;
+        static const AkUniqueID RESET_COWBELL02 = 1735431227U;
+        static const AkUniqueID RESET_CYMBAL01 = 3773451286U;
+        static const AkUniqueID RESET_SNAREDRUM01 = 2907261653U;
+        static const AkUniqueID RESET_SOLO01 = 2676278653U;
+        static const AkUniqueID VI_BASS01 = 1566012757U;
+        static const AkUniqueID VI_BASSDRUM01 = 3991181389U;
+        static const AkUniqueID VI_CHORDS01 = 413502601U;
+        static const AkUniqueID VI_COWBELL01 = 2050860192U;
+        static const AkUniqueID VI_COWBELL02 = 2050860195U;
+        static const AkUniqueID VI_CYMBAL01 = 3201313006U;
+        static const AkUniqueID VI_SNAREDRUM01 = 3546710381U;
+        static const AkUniqueID VI_SOLO01 = 3584147509U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CONTROLLER_BASS = 1315107341U;
+        static const AkUniqueID CONTROLLER_BDRUM = 2721311426U;
+        static const AkUniqueID CONTROLLER_CHORDS = 3573041661U;
+        static const AkUniqueID CONTROLLER_COWBELL = 549677646U;
+        static const AkUniqueID CONTROLLER_COWBELL2 = 3785382136U;
+        static const AkUniqueID CONTROLLER_CYMBAL = 2516872914U;
+        static const AkUniqueID CONTROLLER_SDRUM = 2417179647U;
+        static const AkUniqueID CONTROLLER_SOLO = 4203175449U;
+        static const AkUniqueID MASTER_VOLUME_CONTROL = 2232728930U;
+        static const AkUniqueID VOLUME_CONTROL = 4194816893U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ currentVersion/DrumAndBass_V0.4/README.txt
@@ -0,0 +1,18 @@
+Hit the start button to start the game
+
+The bottom bar shows all the fish you can select. if you click on the fish bubble, it switches out for the props menu.
+
+The top left and right buttons exit the game and reset the scene.
+
+In order to move the camera, click the text saying "Camera:Off", and then left click and drag to move the camera around.
+
+To spawn a fish, click on the fish you wish to spawn through the fish menu.
+To spawn a prop, open the prop menu to select the prop you want, then click on the base of the fish tank to place a prop there.
+To select a fish or prop, left click on it.
+To zoom in on the selected fish or prop, hold Tab.
+To delete a fish or prop, press the delete key with the fish or prop selected.
+To pause the game, press escape.
+
+Fish play music as they come into the scene, and props modify that music in some way.
+there are currently 10 fish and 1 placeholder prop.
+We intend to have a handful more props, that the player can use to modify the oundscape in unique ways.
