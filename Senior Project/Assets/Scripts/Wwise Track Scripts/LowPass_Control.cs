fae14948ea0370edd641342657eeb1458a55f68d
VirtualTitan <superrrgaf@yahoo.com>
2021-03-29 21:46:51 -0400

    Added new effects sscriptes and attached them to props
    -Added scripts that can add high pass and low pass filters based on positioning.
    -Added those scripts as well as the pitch control script to props

---
+++ Senior Project/Assets/Scripts/Wwise Track Scripts/LowPass_Control.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowPass_Control : MonoBehaviour
+{
+    public float lowPVal;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lowPVal = transform.position.x + 50;
+
+        AkSoundEngine.SetRTPCValue("LowPass_Control", lowPVal);
+        Debug.Log("The amount of reverb was set to: " + lowPVal);
+    }
+}
---
+++ Senior Project/Assets/Scripts/Wwise Track Scripts/highPass_Control.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class highPass_Control : MonoBehaviour
+{
+    public float highPVal;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        highPVal = transform.position.x + 50;
+
+        AkSoundEngine.SetRTPCValue("HighPass_Control", highPVal);
+        Debug.Log("The amount of reverb was set to: " + highPVal);
+    }
+}
+
